f2e88fef5c8a34e991b1a6739453ca06d5a8ad5a
vynguyen324 <vivianyn.co@gmail.com>
2024-04-22 17:37:35 -0400

    Added basic form handling and added Typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generateSong(event) {
+  event.preventDefault();
 
+  new Typewriter("#song-lyrics", {
+    strings: "blahblahblah",
+    autoStart: true,
+    delay: 10,
+    cursor: "",
+  });
+}
+
+let songFormElement = document.querySelector("#song-lyrics-form");
+songFormElement.addEventListener("submit", generateSong);
